99643358b56410f557334beaba2499c4de128d49
John Regan <johnaregan@yahoo.co.uk>
2016-03-15 14:15:18 +0000

    new project

---
+++ src/GitConsoleDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitConsoleDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			//changes to project v1
+        }
+    }
+}
